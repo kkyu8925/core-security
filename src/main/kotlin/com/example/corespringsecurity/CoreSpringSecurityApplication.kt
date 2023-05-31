893e05fdc4d6496511fd3072298b53790ed13807
kkyu8925 <kkyu8925@gmail.com>
2023-05-31 22:18:02 +0900

    init

---
+++ src/main/kotlin/com/example/corespringsecurity/CoreSpringSecurityApplication.kt
@@ -0,0 +1,11 @@
+package com.example.corespringsecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoreSpringSecurityApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoreSpringSecurityApplication>(*args)
+}
