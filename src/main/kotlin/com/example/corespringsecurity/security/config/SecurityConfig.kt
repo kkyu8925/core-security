d602bf5fd08e0e93c9fb096affedc0233da58026
kkyu8925 <kkyu8925@gmail.com>
2023-06-07 22:44:35 +0900

    3-2) 정적 자원 관리 - WebIgnore 설정

--- src/main/kotlin/com/example/corespringsecurity/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/corespringsecurity/security/config/SecurityConfig.kt
@@ -1,15 +1,18 @@
 package com.example.corespringsecurity.security.config
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+
 
 @Configuration
 @EnableWebSecurity
@@ -43,6 +46,13 @@ class SecurityConfig {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder()
+    }
+
+    @Bean
+    fun webSecurityCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer {
+            it.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+        }
     }
 
     @Bean
