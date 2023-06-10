70c88c2c3fdae87e03d83fbebd1d407f47911351
kkyu8925 <kkyu8925@gmail.com>
2023-06-10 23:19:29 +0900

    3-7) 로그아웃 및 인증에 따른 화면 보안 처리

--- src/main/kotlin/com/example/corespringsecurity/controller/login/LoginController.kt
+++ src/main/kotlin/com/example/corespringsecurity/controller/login/LoginController.kt
@@ -1,5 +1,9 @@
 package com.example.corespringsecurity.controller.login
 
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
@@ -9,5 +13,12 @@ class LoginController {
     @GetMapping("/login")
     fun login(): String {
         return "user/login/login"
+    }
+
+    @GetMapping("/logout")
+    fun logout(request: HttpServletRequest, response: HttpServletResponse): String {
+        val authentication = SecurityContextHolder.getContext().authentication
+        SecurityContextLogoutHandler().logout(request, response, authentication)
+        return "redirect:/login"
     }
 }
