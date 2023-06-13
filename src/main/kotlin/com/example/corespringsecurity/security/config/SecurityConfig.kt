50163b51a5336da25150d50e6feb6fccdd5e417a
kkyu8925 <kkyu8925@gmail.com>
2023-06-13 22:05:04 +0900

    4-2) 인증 필터 - AjaxAuthenticationFilter

--- src/main/kotlin/com/example/corespringsecurity/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/corespringsecurity/security/config/SecurityConfig.kt
@@ -1,18 +1,25 @@
 package com.example.corespringsecurity.security.config
 
 import com.example.corespringsecurity.security.common.CustomFormWebAuthenticationDetailsSource
+import com.example.corespringsecurity.security.filter.AjaxLoginProcessingFilter
 import com.example.corespringsecurity.security.handler.CustomFormAccessDeniedHandler
 import com.example.corespringsecurity.security.handler.CustomFormAuthenticationFailureHandler
 import com.example.corespringsecurity.security.handler.CustomFormAuthenticationSuccessHandler
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
@@ -23,16 +30,16 @@ class SecurityConfig(
     private val customFormAccessDeniedHandler: CustomFormAccessDeniedHandler
 ) {
 
-//    @Bean
-//    fun userDetailsService(): UserDetailsService {
-//        val password = passwordEncoder().encode("1234")
-//
-//        val user = User.withUsername("user").password(password).roles("USER").build()
-//        val sys = User.withUsername("manager").password(password).roles("MANAGER", "USER").build()
-//        val admin = User.withUsername("admin").password(password).roles("ADMIN", "USER", "MANAGER").build()
-//
-//        return InMemoryUserDetailsManager(user, sys, admin)
-//    }
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val password = passwordEncoder().encode("1111")
+
+        val user = User.withUsername("user").password(password).roles("USER").build()
+        val sys = User.withUsername("manager").password(password).roles("MANAGER", "USER").build()
+        val admin = User.withUsername("admin").password(password).roles("ADMIN", "USER", "MANAGER").build()
+
+        return InMemoryUserDetailsManager(user, sys, admin)
+    }
 
     @Bean
     fun passwordEncoder(): PasswordEncoder {
@@ -49,7 +56,15 @@ class SecurityConfig(
     }
 
     @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
+        return authenticationConfiguration.authenticationManager
+    }
+
+    @Bean
+    fun securityFilterChain(
+        http: HttpSecurity,
+        authenticationConfiguration: AuthenticationConfiguration
+    ): SecurityFilterChain {
         return http.authorizeHttpRequests {
             it.requestMatchers("/", "/users", "user/login/**", "/login*").permitAll()
                 .requestMatchers("/mypage").hasRole("USER")
@@ -66,10 +81,20 @@ class SecurityConfig(
                 .permitAll()
         }.exceptionHandling {
             it.accessDeniedHandler(customFormAccessDeniedHandler)
-        }.csrf {
-            it.disable()
-        }.headers {
-            it.frameOptions { frameOption -> frameOption.disable() }
-        }.build()
+        }.addFilterBefore(
+            ajaxLoginProcessingFilter(authenticationConfiguration), UsernamePasswordAuthenticationFilter::class.java
+        )
+            .csrf {
+                it.disable()
+            }.headers {
+                it.frameOptions { frameOption -> frameOption.disable() }
+            }.build()
+    }
+
+    @Bean
+    fun ajaxLoginProcessingFilter(authenticationConfiguration: AuthenticationConfiguration): AjaxLoginProcessingFilter {
+        val ajaxLoginProcessingFilter = AjaxLoginProcessingFilter()
+        ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManager(authenticationConfiguration))
+        return ajaxLoginProcessingFilter
     }
 }
--- src/main/kotlin/com/example/corespringsecurity/security/filter/AjaxLoginProcessingFilter.kt
+++ src/main/kotlin/com/example/corespringsecurity/security/filter/AjaxLoginProcessingFilter.kt
@@ -1,7 +1,8 @@
 package com.example.corespringsecurity.security.filter
 
-import com.example.corespringsecurity.domain.AccountDto
 import com.example.corespringsecurity.security.token.AjaxAuthenticationToken
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -16,8 +17,10 @@ class AjaxLoginProcessingFilter : AbstractAuthenticationProcessingFilter(AntPath
     override fun attemptAuthentication(request: HttpServletRequest, response: HttpServletResponse): Authentication {
         check(isAjax(request)) { "Authentication is not supported" }
 
-        val (username, password) = objectMapper.readValue(request.reader, AccountDto::class.java)
+        val (username, password) = objectMapper.readValue(request.reader, UsernamePassword::class.java)
+
         require(username.isNotBlank() && password.isNotBlank()) { "Username or Passoword is empty" }
+
         val ajaxAuthenticationToken = AjaxAuthenticationToken(username, password)
         return authenticationManager.authenticate(ajaxAuthenticationToken)
     }
@@ -26,3 +29,7 @@ class AjaxLoginProcessingFilter : AbstractAuthenticationProcessingFilter(AntPath
         return "XMLHttpRequest" == request.getHeader("X-Requested-With")
     }
 }
+
+private data class UsernamePassword @JsonCreator constructor(
+    @JsonProperty("username") val username: String, @JsonProperty("password") val password: String
+)
